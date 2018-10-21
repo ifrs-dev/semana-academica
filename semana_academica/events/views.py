a14f84d842b9ac3ac6124f34e7ad66d38b65a997
eduardohuther <codexico@gmail.com>
2018-10-21 19:48:50 -0300

    event-detail

--- semana_academica/events/views.py
+++ semana_academica/events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 from events.models import Event
 # Create your views here.
 
@@ -8,5 +8,16 @@ class HomeView(ListView):
 	model = Event
 	template_name = 'index.html'
 
+	def get_context_data(self, *args, **kwargs):
+		event = Event.objects.all()
+
 class ProgramTemplateView(TemplateView):
 	template_name = 'program.html'
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'events/event-detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        event = self.get_object()
+        user = self.request.user
--- semana_academica/semana_academica/urls.py
+++ semana_academica/semana_academica/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views_events.HomeView.as_view(), name='home'),
     path('programacao', views_events.ProgramTemplateView.as_view(), name='program'),
+    path('minicurso/<int:pk>/', views_events.EventDetailView.as_view(), name='event-detail'),
 ]
